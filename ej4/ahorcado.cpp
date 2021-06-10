839d8c7675e7ecf2f4392b1ccef653cdc5f24731
Agustin <ratto_agustin@hotmail.com>
2021-06-09 21:26:22 -0300

    version experimental

--- ej4/ahorcado.cpp
+++ ej4/ahorcado.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-#include <stdlib.h>
-#include <unistd.h>
-#include <limits>
+#include "bibliotecas.hpp"
+#include "SharedMemory.cpp"
 using namespace::std;
 
 string inicializar(int cant);
@@ -11,39 +9,40 @@ bool esLetraIngresada(string& letras,char letraBuscada);
 void limpiarPantalla();
 bool esLetra(char letra);
 bool validar(const string& letra);
+string obtenerPalabraDeArchivo();
 
 int main() {
-    string palabra = "probando";
-    string letrasIngresadas = "";
-    string letra;
-    int intentos = 6;
-    int aciertos = 0;
     int cantCambios;
-    string palabraOculta = inicializar(palabra.size());
-    bool res;
-    system("clear");
+    
+    string palabra = obtenerPalabraDeArchivo();
+    SharedMemory sharedMemory(palabra,inicializar(palabra.size()));
 
-    while(intentos > 0 && aciertos < palabra.size()) { 
-        cout << palabraOculta << endl;
+    //system("clear");
+    datos* datosJuego = sharedMemory.getDatos();
+
+
+    while(datosJuego->intentos > 0 && datosJuego->aciertos < palabra.size()) { 
+        cout << datosJuego->palabraOculta << endl;
         do
         {
             cout << "ingrese una letra: ";
-            cin >> letra;
-        } while (!validar(letra));
+            cin >> datosJuego->letra;
+        } while (!validar(datosJuego->letra));
 
-        //validar solo que es letra y no otro caracter raro
-        if(!esLetraIngresada(letrasIngresadas,letra.front())) {
+        datosJuego->letra = tolower(datosJuego->letra[0]);
+
+        if(!esLetraIngresada(datosJuego->letrasIngresadas,datosJuego->letra.front())) {
             
-            cantCambios = buscarApariciones(palabra,palabraOculta,letra.front());
+            cantCambios = buscarApariciones(datosJuego->palabra,datosJuego->palabraOculta,datosJuego->letra.front());
             if(cantCambios > 0) {
-                aciertos += cantCambios;
+                datosJuego->aciertos += cantCambios;
             }
             else {
-                intentos--;
-                if(intentos) {
-                    cout << "la letra " << "\'" << letra << "\'" << " no se encuentra!\n"
-                            "te quedan " << intentos << " vidas" << endl;
-                    //limpiarPantalla();
+                datosJuego->intentos--;
+                if(datosJuego->intentos) {
+                    cout << "la letra " << "\'" << datosJuego->letra << "\'" << " no se encuentra!\n"
+                            "te quedan " << datosJuego->intentos << " vidas" << endl;
+                    limpiarPantalla();
                 }//el else puede que sea innecesario
                 else {
                     system("clear");
@@ -53,16 +52,37 @@ int main() {
         else {
                 cout << "ya ingreso esa letra!" << endl;
 
-                //limpiarPantalla();
+                limpiarPantalla();
             }
     }
 
-    if(aciertos == palabra.size()) {
+    if(datosJuego->aciertos == palabra.size()) {
         cout << "ganaste!" << endl;
     }
     else {
-        cout << "perdiste!" << endl << "La palabra era " << "\'" << palabra << "\'" << endl;
+        cout << "perdiste!" << endl << "La palabra era " << "\'" << datosJuego->palabra << "\'" << endl;
     }
+}
+
+string obtenerPalabraDeArchivo() {
+
+    fstream palabrasFile;
+    srand(time(nullptr));
+    palabrasFile.open("palabras.txt",ios::in);
+
+    if(!palabrasFile) {
+        cerr << "No se pudo abrir archivo" << endl;
+        exit(1);
+    }
+
+    std::vector<string> palabras;
+    string cadena;
+    while(palabrasFile >> cadena) {
+        palabras.push_back(cadena);
+    }
+    palabrasFile.close();
+    
+    return palabras[rand()%palabras.size()];
 }
 
 string inicializar(int cant) {
@@ -108,10 +128,14 @@ bool esLetra(char letra) {
 
 void limpiarPantalla() {
 
-    cout << "Presione enter para continuar... " << endl;
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    cin.get();
+    
+    do 
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Presione enter para continuar... " << endl;
+    } while (cin.get() != '\n');
+
     system("clear");
 }
 
@@ -121,7 +145,7 @@ bool validar(const string& letra) {
         return true;
 
     cout << "Ingrese una sola letra y que sea valida!" << endl;
-    //limpiarPantalla();
+    limpiarPantalla();
     
     return false;
 }
