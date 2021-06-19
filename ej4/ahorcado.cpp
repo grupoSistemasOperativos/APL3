ea5c40eccd5d5377437251561b18a51c42eec2af
agustin <ratto_agustin@hotmail.com>
2021-06-19 05:52:43 -0300

    help agregado

--- ej4/ahorcado.cpp
+++ ej4/ahorcado.cpp
@@ -3,17 +3,45 @@
 void liberarSemaforos();
 void signalHandler(int sig);
 
+bool mostrarAyuda(const char *cad);
 void empezarJuego();
 
 struct sigaction action;
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if(argc > 2)
+    {
+        cerr << "Error: Cantidad de parametros invalida" << endl;
+        exit(1);
+    }
+    if(argc == 2)
+    {
+        if(!mostrarAyuda(argv[1]))
+            cerr << "Error: parametro invalidos" << endl;
+        exit(1);
+    }
 
     empezarJuego();
 
     liberarSemaforos();
 
     return 0;
+}
+
+bool mostrarAyuda(const char *cad)
+{
+    if (!strcmp(cad, "-h") || !strcmp(cad, "--help") )
+    {
+        cout << "NAME" << endl;
+        cout << "    servidor - realiza logica del ahorcado" << endl;
+        cout << "SYNOPSIS:" << endl;
+        cout << "    servidor [--help -h]" << endl;
+        cout << "DESCRIPTION:" << endl;
+        cout << "    Encargado de ejecutar la logica en base al cliente que se conecte" << endl;
+        return true;
+    }
+    return false;
 }
 
 void empezarJuego() {
@@ -124,40 +152,40 @@ void signalHandler(int sig) {
     switch (sig)
     {
     case SIGUSR1:
-            if(seEjecutaCliente()) {
-                cout << "No se puede finalizar el servidor si hay un cliente ejecutandose" << endl;
-            }
-            else {
-                cout << "Cerrando servidor..." << endl;
-                liberarMemoriaCompartida();
-                liberarSemaforos();
-                shm_unlink(memoriaCompartida);
-                kill(getpid(),SIGTERM);
-            }
+                {
+                    if(seEjecutaCliente()) {
+                        cout << "No se puede finalizar el servidor si hay un cliente ejecutandose" << endl;
+                    }
+                    else {
+                        cout << "Cerrando servidor..." << endl;
+                        liberarMemoriaCompartida();
+                        liberarSemaforos();
+                        shm_unlink(memoriaCompartida);
+                        kill(getpid(),SIGTERM);
+                    }
+                }
         break;
     case SIGUSR2:
-            cout << "finalizando partida..." << endl;
-            sem_t *esperarFinalizacion = sem_open("finalizacion",O_CREAT,0600,0);
+            {
+                cout << "finalizando partida..." << endl;
+                sem_t *esperarFinalizacion = sem_open("finalizacion",O_CREAT,0600,0);
 
-            sem_wait(esperarFinalizacion);
+                sem_wait(esperarFinalizacion);
 
-            liberarSemaforos();
-            liberarMemoriaCompartida();
-            shm_unlink(memoriaCompartida);
+                liberarSemaforos();
+                liberarMemoriaCompartida();
+                shm_unlink(memoriaCompartida);
 
-            system("clear");
-            sem_unlink("finalizacion");
-            empezarJuego();
+                system("clear");
+                sem_unlink("finalizacion");
+                empezarJuego();
+            }
         break;
     }
 }
 
 void liberarSemaforos() {
-
-    sem_unlink("empezarJuego");
-    sem_unlink("esperarLetra");
-    sem_unlink("busquedaLetra");
-    sem_unlink("verificarFin");
+    sem_unlink("finalizacion");
     sem_unlink("cliente");
     sem_unlink("servidor");
 }
--- ej4/cliente.cpp
+++ ej4/cliente.cpp
@@ -4,9 +4,22 @@
 #define YA_INGRESADA -1
 #define GANA 1
 
+bool mostrarAyuda(const char *cad);
 void signalHandler(int sig);
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if(argc > 2)
+    {
+        cerr << "Error: Cantidad de parametros invalida" << endl;
+        exit(1);
+    }
+    if(argc == 2)
+    {
+        if(!mostrarAyuda(argv[1]))
+            cerr << "Error: parametro invalidos" << endl;
+        exit(1);
+    }
 
     if(!validarEjecucionServidor()) {
         cerr << "El servidor no es esta ejecutando" << endl;
@@ -20,7 +33,6 @@ int main() {
 
     string letra;
 
-    sem_t *verificarFin = sem_open("verificarFin", O_CREAT, 0600, 0);
     sem_t *cliente = sem_open("cliente", O_CREAT, 0600, 0);
     sem_t *servidor = sem_open("servidor",O_CREAT,0600,0);
     
@@ -86,6 +98,22 @@ int main() {
     liberarMemoriaCompartida();
 
     return 0;
+}
+
+bool mostrarAyuda(const char *cad)
+{
+    if (!strcmp(cad, "-h") || !strcmp(cad, "--help") )
+    {
+        cout << "NAME" << endl;
+        cout << "    cliente - ejecutable para iniciar ahorcado" << endl;
+        cout << "SYNOPSIS:" << endl;
+        cout << "    cliente [--help -h]" << endl;
+        cout << "DESCRIPTION:" << endl;
+        cout << "    Inicie el ejecutable para poder jugar al ahorcado" << endl;
+        cout << "Aclaracion: el servidor debe estar corriendo previamente" << endl;
+        return true;
+    }
+    return false;
 }
 
 void signalHandler(int sig) {
